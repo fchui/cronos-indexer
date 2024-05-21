6bafd5de0c10419bc4638fc4a482c8a8659c3929
Francis C <fchui@sfu.ca>
2024-05-20 23:18:53 -0700

    Simple connection to get initial block number on refresh

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import axios from 'axios';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+function errorMessage(error: unknown) {
+  if (error instanceof Error)
+    return console.log(error.message)
+  return console.log(String(error))
+}
+
+async function fetchUsers() {
+  try {
+    const response = await axios.get(`https://explorer-api.cronos.org/mainnet/api/v1/ethproxy/getBlockNumber?apikey=${process.env.APIKEY}`);
+    const users = response.data;
+    console.log('Initial block number:');
+    console.log(users.result);
+    console.log(parseInt(users.result.substring(2),16))
+  } catch (error) {
+    errorMessage(error)
+  }
+}
+
+const myLogger = function (req: Request, res: Response, next: Function) {
+  console.log('LOGGED')
+  fetchUsers();
+}
+
+app.use(myLogger)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
